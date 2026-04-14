ef5ad96a4af3814db1d729178a59b9d9eef6957c
ameerhakim <ameerhakim@ufl.edu>
2026-04-14 10:52:14 -0400

    Might be complete?

--- rtl/bnn.sv
+++ rtl/bnn.sv
@@ -44,8 +44,8 @@ module bnn #(
   input logic bnn_valid_in,
 
   // OUTPUTS
-  output logic [LAYER_PN[TOTAL_LAYERS-2]-1:0] bnn_popcounts [LAYER_PN[TOTAL_LAYERS-1]],
-  output logic [LAYER_PN[TOTAL_LAYERS-1]-1:0] bnn_data_out,
+  output logic [$clog2(LAYER_PN[2]+1)-1:0] bnn_popcounts [LAYER_PN[3]],
+  output logic [LAYER_PN[3]-1:0] bnn_data_out,
   output logic bnn_last,
   output logic bnn_valid_out
 );
@@ -56,6 +56,16 @@ module bnn #(
   logic [TOTAL_LAYERS-1:0] layer_go_out;
   logic [TOTAL_LAYERS-1:0] layer_valid_out;
   logic [TOTAL_LAYERS-1:0] layer_last;
+
+  // WASTED TOO MUCH TIME TRYING TO GET GENERATE TO CLEANLY MAP SIGNALS
+  // DOING IT THIS INELOQUENT WAY FOR MY SANITY
+  logic [$clog2(TOPOLOGY[0]+1)-1:0] layer1_pc [LAYER_PN[1]];
+  logic [$clog2(LAYER_PN[1]+1)-1:0] layer2_pc [LAYER_PN[2]];
+  logic [$clog2(LAYER_PN[2]+1)-1:0] layer3_pc [LAYER_PN[3]];
+
+  logic [LAYER_PN[1]-1:0] layer1_data;
+  logic [LAYER_PN[2]-1:0] layer2_data;
+  logic [LAYER_PN[3]-1:0] layer3_data;
 
   // INPUT LAYER
   logic input_go_out_r;
@@ -99,78 +109,134 @@ module bnn #(
 
   // INSTANTIATE LAYERS
   //// HIDDEN LAYERS
-  for (genvar i=1; i<TOTAL_LAYERS; i++) begin : g_layers
+  for (genvar i=1; i<TOTAL_LAYERS; i++) begin : g_config
 
     // CONFIG DEMUX
     logic cl_mode_i;
     logic cl_t_we_i;
     logic cl_w_we_i;
     
-    if (i == 0) begin
-      assign cl_mode_i = '0;
-      assign cl_w_we_i = '0;
-      assign cl_t_we_i = '0;
-  
-    end else begin
-      assign cl_mode_i = cl_mode_i && (layer_sel == i);
-      assign cl_w_we_i = cl_w_we_i && (layer_sel == i);
-      assign cl_t_we_i = cl_t_we_i && (layer_sel == i);
-
-    end
-
-    // LAYER POPCOUNT
-    if (i==1) begin
-      logic [$clog2(TOPOLOGY[0]+1)-1:0] layer_pc [LAYER_PN[i]]; 
-    end else begin
-      logic [$clog2(LAYER_PN[i-1]+1)-1:0] layer_pc [LAYER_PN[i]];
-    end
-
-    // LAYER DATA
-    logic [LAYER_PN[i]-1:0] layer_data;
-
-    layer #(
-      // PARAMETERS
-      .LAYER_ID   (i),
-      .NUM_INPUTS ((i==1) ? TOPOLOGY[0] : LAYER_PN[i-1]),
-      .NUM_NEURONS(TOPOLOGY[i]),
-      .P_N        (LAYER_PN[i])
-    ) layers (
-      // CLK AND RST
-      .clk(clk),
-      .rst(rst),
-    
-      // INPUTS
-      //// CONFIGURATION INTERFACE
-      .config_mode  (cl_mode_i),
-      .config_w_we  (cl_w_we_i),
-      .config_t_we  (cl_t_we_i),
-      .config_w_addr(config_w_addr),
-      .config_t_addr(config_t_addr),
-      .config_w_data(config_w_data),
-      .config_t_data(config_t_data),
-      .config_np_id (config_np_id),
-      
-      //// DATA
-      .layer_en(layer_en[i]),
-      .layer_valid_in((i==1) ? input_valid_out_r : layer_valid_out[i-1]),
-      .layer_go_in   ((i==1) ? input_go_out_r    : layer_go_out[i-1]),
-      .layer_data_in ((i==1) ? input_data_out_r  : g_layers[i-1].layer_data),
-    
-      // OUTPUTS
-      .layer_go_out   (layer_go_out[i]),
-      .layer_ready    (layer_ready[i]),
-      .layer_last     (layer_last[i]),
-      .layer_valid_out(layer_valid_out[i]),
-      .layer_data_out (layer_data),
-      .layer_popcounts(layer_pc)
-    );
+    assign cl_mode_i = config_mode && (layer_sel == i);
+    assign cl_w_we_i = config_w_we && (layer_sel == i);
+    assign cl_t_we_i = config_t_we && (layer_sel == i);
 
   end
 
+  layer #(
+    // PARAMETERS
+    .LAYER_ID   (1),
+    .NUM_INPUTS (TOPOLOGY[0]),
+    .NUM_NEURONS(TOPOLOGY[1]),
+    .P_N        (LAYER_PN[1])
+  ) layer1 (
+    // CLK AND RST
+    .clk(clk),
+    .rst(rst),
+  
+    // INPUTS
+    //// CONFIGURATION INTERFACE
+    .config_mode  (g_config[1].cl_mode_i),
+    .config_w_we  (g_config[1].cl_w_we_i),
+    .config_t_we  (g_config[1].cl_t_we_i),
+    .config_w_addr(config_w_addr),
+    .config_t_addr(config_t_addr),
+    .config_w_data(config_w_data),
+    .config_t_data(config_t_data),
+    .config_np_id (config_np_id),
+    
+    //// DATA
+    .layer_en(layer_en[1]),
+    .layer_valid_in(input_valid_out_r),
+    .layer_go_in   (input_go_out_r),
+    .layer_data_in (input_data_out_r),
+  
+    // OUTPUTS
+    .layer_go_out   (layer_go_out[1]),
+    .layer_ready    (layer_ready[1]),
+    .layer_last     (layer_last[1]),
+    .layer_valid_out(layer_valid_out[1]),
+    .layer_data_out (layer1_data),
+    .layer_popcounts(layer1_pc)
+  );
+
+  layer #(
+    // PARAMETERS
+    .LAYER_ID   (2),
+    .NUM_INPUTS (LAYER_PN[1]),
+    .NUM_NEURONS(TOPOLOGY[2]),
+    .P_N        (LAYER_PN[2])
+  ) layer2 (
+    // CLK AND RST
+    .clk(clk),
+    .rst(rst),
+  
+    // INPUTS
+    //// CONFIGURATION INTERFACE
+    .config_mode  (g_config[2].cl_mode_i),
+    .config_w_we  (g_config[2].cl_w_we_i),
+    .config_t_we  (g_config[2].cl_t_we_i),
+    .config_w_addr(config_w_addr),
+    .config_t_addr(config_t_addr),
+    .config_w_data(config_w_data),
+    .config_t_data(config_t_data),
+    .config_np_id (config_np_id),
+    
+    //// DATA
+    .layer_en(layer_en[2]),
+    .layer_valid_in(layer_valid_out[1]),
+    .layer_go_in   (layer_go_out[1]),
+    .layer_data_in (layer1_data),
+  
+    // OUTPUTS
+    .layer_go_out   (layer_go_out[2]),
+    .layer_ready    (layer_ready[2]),
+    .layer_last     (layer_last[2]),
+    .layer_valid_out(layer_valid_out[2]),
+    .layer_data_out (layer2_data),
+    .layer_popcounts(layer2_pc)
+  );
+
+  layer #(
+    // PARAMETERS
+    .LAYER_ID   (3),
+    .NUM_INPUTS (LAYER_PN[2]),
+    .NUM_NEURONS(TOPOLOGY[3]),
+    .P_N        (LAYER_PN[3])
+  ) layer3 (
+    // CLK AND RST
+    .clk(clk),
+    .rst(rst),
+  
+    // INPUTS
+    //// CONFIGURATION INTERFACE
+    .config_mode  (g_config[3].cl_mode_i),
+    .config_w_we  (g_config[3].cl_w_we_i),
+    .config_t_we  (g_config[3].cl_t_we_i),
+    .config_w_addr(config_w_addr),
+    .config_t_addr(config_t_addr),
+    .config_w_data(config_w_data),
+    .config_t_data(config_t_data),
+    .config_np_id (config_np_id),
+    
+    //// DATA
+    .layer_en(layer_en[3]),
+    .layer_valid_in(layer_valid_out[2]),
+    .layer_go_in   (layer_go_out[2]),
+    .layer_data_in (layer2_data),
+  
+    // OUTPUTS
+    .layer_go_out   (layer_go_out[3]),
+    .layer_ready    (layer_ready[3]),
+    .layer_last     (layer_last[3]),
+    .layer_valid_out(layer_valid_out[3]),
+    .layer_data_out (layer3_data),
+    .layer_popcounts(layer3_pc)
+  );
+
   // ASSIGN OUTPUTS
-  assign bnn_data_out  = g_layers[TOTAL_LAYERS-1].layer_data;
-  assign bnn_popcounts = g_layers[TOTAL_LAYERS-1].layer_pc;
-  assign bnn_last      = layer_last[TOTAL_LAYERS-1];
-  assign bnn_valid_out = layer_valid_out[TOTAL_LAYERS-1];
+  assign bnn_data_out  = layer3_data;
+  assign bnn_popcounts = layer3_pc;
+  assign bnn_last      = layer_last[3];
+  assign bnn_valid_out = layer_valid_out[3];
 
 endmodule
